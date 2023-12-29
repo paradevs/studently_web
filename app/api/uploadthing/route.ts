73acaa7afd32e20796f4ff8ac399da4737ed67ff
Para <140919089+paradevs@users.noreply.github.com>
2023-12-29 19:40:15 +0300

    commented const config error

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -6,13 +6,13 @@ import { createNextRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '5mb',
-    },
-  },
-}
+// export const config = {
+//   api: {
+//     bodyParser: {
+//       sizeLimit: '5mb',
+//     },
+//   },
+// }
 
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
